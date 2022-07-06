b2ab923ac3e242244838b3469cdcfc06d723f92d
Dilip Kumar <63644683+dvukumar@users.noreply.github.com>
2022-07-06 10:47:00 +0530

    Time: 1269 ms (9.71%), Space: 13.8 MB (54.45%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        
+        def f(n):
+            if n<=1:
+                return n
+            return f(n-1)+f(n-2)
+            
+        return f(n)
