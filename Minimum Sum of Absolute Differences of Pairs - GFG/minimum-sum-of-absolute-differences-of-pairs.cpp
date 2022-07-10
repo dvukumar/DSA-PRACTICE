e701c96f864ab0b1c125b5484a5c91736e131a98
Dilip Kumar <63644683+dvukumar@users.noreply.github.com>
2022-07-11 00:36:57 +0530

    Added solution - LeetHub

---
+++ Minimum Sum of Absolute Differences of Pairs - GFG/minimum-sum-of-absolute-differences-of-pairs.cpp
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    long long findMinSum(vector<int> &A,vector<int> &B,int N){
+        sort(A.begin(),A.end());
+        sort(B.begin(),B.end());
+        
+        long long Sum = 0;
+        
+        for(long long i=0;i<N;i++){
+            long long diff = A[i] - B[i];
+            if(diff<0)
+                diff = diff*-1;
+            Sum = Sum+diff;
+        }
+        
+        return Sum;
+        
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        vector<int> A(N),B(N);
+        for(int i=0;i<N;i++){
+            cin>>A[i];
+        }
+        for(int i=0;i<N;i++){
+            cin>>B[i];
+        }
+        Solution ob;
+        cout<<ob.findMinSum(A,B,N)<<endl;
+    }
+}  // } Driver Code Ends
