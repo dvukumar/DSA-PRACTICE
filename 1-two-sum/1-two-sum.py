2084423377a868f18fef3288403ddaa1fa1e0cb4
Dilip Kumar <63644683+dvukumar@users.noreply.github.com>
2022-07-19 17:47:07 +0530

    Time: 1644 ms (28.29%), Space: 14.9 MB (95.36%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,45 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
-        arr = [i for i in nums]
-        arr.sort() #O(NlogN)
-        
-        def BinarySearch(arr,target):
-            l = 0
-            r = len(arr)-1
-            
-            while(l<=r):
-                mid = (l+r)//2
-                if target==arr[mid]:
-                    return target
-                elif target>arr[mid]:
-                    l = mid+1
-                else:
-                    r = mid-1
-            return 10**10
-            
-        
-        for i in range(len(arr)):
-            temp = arr.pop()
-            
-            new = target-temp
-            
-            find = BinarySearch(arr,new)
-            
-            if find == 10**10:
-                continue
+        for i in range(len(nums)):
+            arr = []
+            a = nums[i]
+            nums.pop(i)
+            temp = target-a
+            if temp in nums:
+                arr.append(i)
+                j = nums.index(temp)
+                arr.append(j+1)
+                return arr
             else:
-                break
-        print(temp,find)
-        idx1 = nums.index(temp)
-        nums[idx1] = 10**10
-        idx2 = nums.index(find)
-        
-        if idx1<idx2:
-            return [idx1,idx2]
-        else:
-            return [idx2,idx1]
-        
-        
-            
-        
+                nums.insert(i,a)
