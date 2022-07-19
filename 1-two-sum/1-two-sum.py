5ba0b7795aba3da18044f971e8d03467ba75ef17
Dilip Kumar <63644683+dvukumar@users.noreply.github.com>
2022-07-19 19:10:40 +0530

    Time: 137 ms (38.14%), Space: 16 MB (9.23%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,15 +1,37 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
+        arr = [i for i in nums]
+        arr.sort() #O(NlogN)
+        dict = {}
+        
         for i in range(len(nums)):
-            arr = []
-            a = nums[i]
-            nums.pop(i)
-            temp = target-a
-            if temp in nums:
-                arr.append(i)
-                j = nums.index(temp)
-                arr.append(j+1)
-                return arr
+            val = nums[i]
+            if val in dict:
+                dict[val].append(i)
             else:
-                nums.insert(i,a)
+                dict[val] = [i]
+        
+        
+        for i in range(len(nums)):
+            first = nums[i]
+            second = target - first
+            
+            if second in dict:
+                
+                if first==second:
+                    if len(dict[first])>1:
+                        idx1 = dict[first][0]
+                        idx2 = dict[first][1]
+                        break
+                else:
+                    idx2 = dict[second][0]
+                    idx1 = dict[first][0]
+                    break
+
+                
+                
+        
+        print(dict)
+        return [idx1,idx2]
+        
