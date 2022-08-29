58504477d29a2c4feb8ecf518e4949fbf93c3f60
Dilip Kumar <63644683+dvukumar@users.noreply.github.com>
2022-08-29 23:55:44 +0530

    Time: 494 ms (49.39%), Space: 16.6 MB (50.32%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,21 +1,26 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def dfs(row,col,grid):
-            if row<0 or col<0 or row>=len(grid) or col>=len(grid[0]):
-                return
-            if grid[row][col]=="0":
-                return
-            grid[row][col] = "0"
-            dfs(row+1,col,grid)
-            dfs(row-1,col,grid)
-            dfs(row,col+1,grid)
-            dfs(row,col-1,grid)
+        n,m = len(grid),len(grid[0])
+        
+        def island(grid,i,j):
+            if i<0 or i>=len(grid) or j<0 or j>=len(grid[0]) or grid[i][j]=="0":
+                return 1
+            grid[i][j] = "0"
+            left = island(grid,i,j-1)
+            right = island(grid,i,j+1)
+            up = island(grid,i-1,j)
+            down = island(grid,i+1,j)
+            
+            if left and right and up and down:
+                
+                return True
         
         count = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        #print(n,m)
+        for i in range(n):
+            for j in range(m):
                 if grid[i][j]=="1":
                     count+=1
-                    dfs(i,j,grid)
-        
+                    island(grid,i,j)
+        #print(grid)
         return count
