75f24e579b24dec178571b7e4cba3982001b74c3
Dilip Kumar <63644683+dvukumar@users.noreply.github.com>
2022-08-30 13:00:42 +0530

    Time: 87 ms (95.91%), Space: 15.5 MB (48.05%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,43 +1,44 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         
-        def findIndex(arr,target):
-            l = 0
-            r = len(arr)-1
-            res = []
-            check = []
-            while(l<=r):
-                mid = (l+r)//2
-                
-                if arr[mid]==target:
-                    check.append(mid)
-                    r = mid-1
-                elif arr[mid]>target:
-                    r = mid-1
+        res = []
+        l = 0
+        r = len(nums)-1
+        
+        while(l<=r):
+            mid = (l+r)//2
+            if nums[mid]==target:
+                if mid>0 and nums[mid-1]==target:
+                    r = mid - 1
                 else:
-                    l = mid+1
-            if len(check)==0:
-                res.append(-1)
-            else:
-                minInd = min(check)
-                res.append(minInd)   
-            check.clear()
-            l = 0
-            r = len(arr)-1
-            while(l<=r):
-                mid = (l+r)//2
-                if arr[mid]==target:
-                    check.append(mid)
-                    l = mid+1
-                elif arr[mid]>target:
-                    r = mid-1
-                else:
-                    l = mid+1
-            if len(check)==0:
-                res.append(-1)
-            else:
-                maxInd = max(check)
-                res.append(maxInd)   
+                    res.append(mid)
+                    break
+            elif nums[mid]>target:
+                r = mid - 1
             
-            return res
-        return findIndex(nums,target)
+            else:
+                l = mid+1
+        
+        if len(res)==0:
+            res.append(-1)
+            
+        l = 0
+        r = len(nums)-1
+        
+        while(l<=r):
+            mid = (l+r)//2
+            if nums[mid]==target:
+                if mid<len(nums)-1 and nums[mid+1]==target:
+                    l = mid + 1
+                else:
+                    res.append(mid)
+                    break
+            elif nums[mid]>target:
+                r = mid - 1
+            
+            else:
+                l = mid+1
+        
+        if len(res)==1:
+            res.append(-1)
+        return res
