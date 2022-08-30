dac5943a20e96bd97d46cd15535e4de41efcf0e0
Dilip Kumar <63644683+dvukumar@users.noreply.github.com>
2022-08-30 22:19:12 +0530

    Time: 191 ms (59.16%), Space: 14.9 MB (41.44%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,36 +1,14 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         
-        arr = []
-        res = []
-        def BinarySearch(arr,target):
-            l = 0
-            r = len(arr)-1
-            
-            while(l<=r):
-                mid = (l+r)//2
-                if arr[mid]==target:
-                    return mid
-                if arr[mid]>target:
-                    r = mid - 1
-                else:
-                    l = mid + 1
-            return -10000
+        l = 0
+        r = len(numbers) - 1
         
-        for i in range(len(numbers)):
-            num = numbers[i]
-            # if num>target:
-            #     arr.append(num)
-            #     continue
-            
-            n = target - num
-            
-            j = BinarySearch(arr,n)
-            if j==-10000:
-                arr.append(num)
+        while(l<=r):
+            sum = numbers[l]+numbers[r]
+            if sum==target:
+                return [l+1,r+1]
+            if sum<target:
+                l += 1
             else:
-                res.append(j+1)
-                res.append(i+1)
-                break
-        return res
-        
+                r -= 1
