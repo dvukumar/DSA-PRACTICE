8ae0192a38b800d98b26260d54850baa744691ba
Dilip Kumar <63644683+dvukumar@users.noreply.github.com>
2022-09-11 12:21:19 +0530

    Time: 35 ms (86.44%), Space: 13.9 MB (29.30%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -6,25 +6,47 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        arr1 = []
-        arr2 = []
-        def findSame(p,arr):
-            if p is None:
-                arr.append(False)
-                return arr
-            else:
-                arr.append(p.val)
-                left = findSame(p.left,arr)
-                right = findSame(p.right,arr)
-                return arr
-        val1 = findSame(p,arr1)
-        val2 = findSame(q,arr2)
-        print(val1,val2)
-        return val1==val2
-                
+        
+        if not p and not q:
+            return True
+        if p and not q:
+            return False
+        if not p and q:
+            return False
+        queue1 = [p]
+        queue2 = [q]
+        
+        while(queue1 and queue2):
+            node1 = queue1.pop()
+            node2 = queue2.pop()
             
+            if node1.val!=node2.val:
+                return False
             
-
+            if node1.left and not node2.left:
+                return False
+            if not node1.left and node2.left:
+                return False
+            
+            if node1.left and node2.left:
+                queue1.append(node1.left)
+                queue2.append(node2.left)
+            
+            if node1.right and not node2.right:
+                return False
+            if not node1.right and node2.right:
+                return False
+            
+            if node1.right and node2.right:
+                queue1.append(node1.right)
+                queue2.append(node2.right)
+        
+        if queue1 and not queue2:
+            return False
+        if not queue1 and queue2:
+            return False
+        
+        return True
 
             
         
