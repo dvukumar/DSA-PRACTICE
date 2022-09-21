00138ae5f049b974aba2271d3d25e75c21d1800e
Dilip Kumar <63644683+dvukumar@users.noreply.github.com>
2022-09-22 00:36:17 +0530

    Time: 67 ms (7.28%), Space: 13.8 MB (72.14%) - LeetHub

--- 1507-reformat-date/1507-reformat-date.py
+++ 1507-reformat-date/1507-reformat-date.py
@@ -1,50 +1,13 @@
 class Solution:
     def reformatDate(self, date: str) -> str:
         
-        dict = {"Jan":"01",
-                "Feb":"02",
-                "Mar":"03",
-                "Apr":"04",
-                "May":"05",
-                "Jun":"06",
-                "Jul":"07",
-                "Aug":"08",
-                "Sep":"09",
-                "Oct":"10",
-                "Nov":"11",
-                "Dec":"12"
-        }
+        months_dict = {"Jan": "01", "Feb":"02", "Mar":"03", "Apr":"04", "May":"05", "Jun":"06", "Jul":"07", "Aug":"08", "Sep":"09", "Oct":"10", "Nov":"11", "Dec":"12"}
+        parts = date.split(' ')
         
-        arr = []
-        num = 1
-        d = ""
-        month = ""
-        year = ""
-        count = 0
-        for char in date:
-            if char==" ":
-                if num==1:
-                    if count<2:
-                        d = '0'+d
-                    arr.append(str(d))
-                elif num==2:
-                    arr.append(dict[month])
-                num+=1
-            elif num==1:
-                digit = ord(char) - ord('0')
-                
-               
-                if digit<10:
-                    d += char
-                    count+=1
-            elif num==2:
-                month += char
-            else:
-                year += char
-        arr.append(year)
-        arr.reverse()
+        parts[1] = months_dict[parts[1]]
+        parts[0] = parts[0][:-2]
         
-        res = ""
-        res = arr[0]+"-"+arr[1]+"-"+arr[2]
-        return res
-                
+        if len(parts[0]) == 1:
+            parts[0] = '0' + parts[0]
+        
+        return '-'.join(reversed(parts))
