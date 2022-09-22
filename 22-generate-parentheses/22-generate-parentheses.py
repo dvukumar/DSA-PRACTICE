56d513a2f1946109aa592d3ac92accbad707adf1
Dilip Kumar <63644683+dvukumar@users.noreply.github.com>
2022-09-22 15:54:20 +0530

    Time: 30 ms (98.63%), Space: 14.3 MB (39.39%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -1,13 +1,22 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         result = []
-        def generate(Open,Close,curr):
-            if Open<0 or Close<0 or Close<Open:
+        stack = []
+        def dp(openP,closeP):
+            if openP==closeP==n:
+                res = "".join(stack)
+                result.append(res)
                 return
-            if Open==Close and Open==0:
-                result.append("".join(curr))
-                return
-            generate(Open-1,Close,curr+['('])
-            generate(Open,Close-1,curr+[')'])
-        generate(n,n,[])
+            
+            if openP<n:
+                stack.append('(')
+                dp(openP+1,closeP)
+                stack.pop()
+            if closeP<openP:
+                stack.append(')')
+                dp(openP,closeP+1)
+                stack.pop()
+        dp(0,0)
         return result
+
+        
