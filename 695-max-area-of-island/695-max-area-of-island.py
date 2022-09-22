1aed0ff88073f8bc04ed897e4a399a43a65ef95f
Dilip Kumar <63644683+dvukumar@users.noreply.github.com>
2022-09-22 16:13:39 +0530

    Time: 287 ms (30.30%), Space: 17.4 MB (41.19%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -11,22 +11,29 @@ class Solution:
             return True
         
         dict = {}
-        def Island(row,col):
+        def isIsland(row,col):
+            
             if (row,col) in dict:
                 return 0
             if isTrue(row,col)==False:
                 return 0
+            
             if grid[row][col]==0:
                 return 0
             dict[(row,col)] = 1
             
-            left,right = Island(row,col-1),Island(row,col+1)
-            up,down = Island(row-1,col),Island(row+1,col)
+            left = isIsland(row,col-1)
+            right = isIsland(row,col+1)
+            up = isIsland(row-1,col)
+            down =isIsland(row+1,col)
+            
             return 1+left+right+up+down
         
         maxArea = 0
         for i in range(m):
             for j in range(n):
-                Area = Island(i,j)
-                maxArea = max(Area,maxArea)
+                if grid[i][j]==0:
+                    continue
+                area = isIsland(i,j)
+                maxArea = max(maxArea,area)
         return maxArea
