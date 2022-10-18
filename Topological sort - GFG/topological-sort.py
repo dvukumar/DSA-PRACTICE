3751f68656e604761d7b267fc6f69a7eae3cf71b
Dilip Kumar <63644683+dvukumar@users.noreply.github.com>
2022-10-18 23:21:34 +0530

    Added solution - LeetHub

---
+++ Topological sort - GFG/topological-sort.py
@@ -0,0 +1,64 @@
+class Solution:
+    
+    #Function to return list containing vertices in Topological order.
+    def topoSort(self, V, adj):
+        # Code here
+        vis = [0]*V
+        res = []
+        
+        def dfs(adj,i,vis,res):
+            vis[i]=1
+            for num in adj[i]:
+                if vis[num]==0:
+                    dfs(adj,num,vis,res)
+                
+            res.append(i)
+        for i in range(V):
+            if vis[i]==0:
+                dfs(adj,i,vis,res)
+        res.reverse()
+        return res
+
+
+
+
+#{ 
+ # Driver Code Starts
+# Driver Program
+
+import sys
+sys.setrecursionlimit(10**6)
+        
+def check(graph, N, res):
+    if N!=len(res):
+        return False
+    map=[0]*N
+    for i in range(N):
+        map[res[i]]=i
+    for i in range(N):
+        for v in graph[i]:
+            if map[i] > map[v]:
+                return False
+    return True
+
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        e,N = list(map(int, input().strip().split()))
+        adj = [[] for i in range(N)]
+        
+        for i in range(e):
+            u,v=map(int,input().split())
+            adj[u].append(v)
+            
+        ob = Solution()
+        
+        res = ob.topoSort(N, adj)
+        
+        if check(adj, N, res):
+            print(1)
+        else:
+            print(0)
+# Contributed By: Harshit Sidhwa
+
+# } Driver Code Ends
